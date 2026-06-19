6e82ab6d60392c756b7f34222a9329960a081bfb
Sentharion <michpawbert@gmail.com>
2026-06-19 22:36:20 +0200

    smallFix

--- app/panelProwadzacego/page.tsx
+++ app/panelProwadzacego/page.tsx
@@ -4,7 +4,7 @@ const PanelProwadzacego = () => {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen w-full py-10 overflow-x-hidden' >
       <Link href="\"> <span className="absolute top-4 left-4 sm:top-10 sm:left-10 text-xl font-bold p-3 rounded-lg flex items-center justify-center gap-2 cursor-pointer hover:scale-110 duration-300"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7" /><path d="M19 12H5" /></svg>Powrót</span></Link>
-      <h2 className='text-2xl sm:text-4xl font-bold mb-12 text-transform: uppercase'>
+      <h2 className='text-2xl sm:text-4xl font-bold mb-9 text-transform: uppercase'>
         Wybierz etap
       </h2>
       <div className='flex flex-col sm:flex-row gap-6 sm:gap-14 justify-center items-center'>
